18e872055615dccc87b8c268a87a85f1cd6754cb
bess-jonas <kontakt@bess-jonas.me>
2026-05-19 09:31:43 +0200

    Addded updateTask and delTask + Task setter Methods

--- src/main/java/com/example/demo/Task.java
+++ src/main/java/com/example/demo/Task.java
@@ -24,4 +24,7 @@ public class Task {
     public UUID getUUID() {return id;}
     public String getTitle() {return title;}
     public boolean isDone() {return done;}
+
+    public void setTitle(String title) {this.title = title;}
+    public void setDone(boolean done) {this.done = done;}
 }
--- src/main/java/com/example/demo/TaskController.java
+++ src/main/java/com/example/demo/TaskController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/tasks")
@@ -21,5 +22,23 @@ public class TaskController {
     public Task createTask(@RequestBody Task task) {
         // save() returns the saved object, now with its auto-generated UUID
         return taskRepository.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable UUID id, @RequestBody Task updatedTask) {
+        //@PathVariable associates id with {id} because of the same name
+        //@RequestBody translates incoming json data from Front-end changes back to Task Object
+
+        // Find the existing task by ID, update its fields, save and return it
+        Task task = taskRepository.findById(id).orElseThrow();
+        task.setTitle(updatedTask.getTitle());
+        task.setDone(updatedTask.isDone());
+        return taskRepository.save(task);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void delTask(@PathVariable UUID id){
+        taskRepository.deleteById(id);
     }
 }
